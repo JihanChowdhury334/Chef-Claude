463ae21049b10030b81cdbf6897fd2c3a8daa150
jihan chowdhury <jihanchowdhury334@gmail.com>
2025-09-19 04:41:31 -0400

    Initial commit - AI Recipe Generator

--- Main.jsx
+++ Main.jsx
@@ -1,28 +1,27 @@
 import React from "react"
+import IngredientsList from "./components/IngredientsList"
+import ClaudeRecipe from "./components/ClaudeRecipe"
+import { getRecipeFromChefClaude, getRecipeFromMistral } from "./ai"
 
 export default function Main() {
+    const [ingredients, setIngredients] = React.useState(
+        ["chicken", "all the main spices", "corn", "heavy cream", "pasta"]
+    )
+    const [recipe, setRecipe] = React.useState("")
 
-    const [ingredients, setIngredients] = React.useState([])
+    async function getRecipe() {
+        const recipeMarkdown = await getRecipeFromChefClaude(ingredients)
+        setRecipe(recipeMarkdown)
+    }
 
-    const ingredientsListItems = ingredients.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
-    ))
-    
-    /**
-     * Challenge: use form action instead of onSubmit to
-     * handle the data from the form
-     */
-
-    function handleSubmit(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
 
     return (
         <main>
-            <form onSubmit={handleSubmit} className="add-ingredient-form">
+            <form action={addIngredient} className="add-ingredient-form">
                 <input
                     type="text"
                     placeholder="e.g. oregano"
@@ -31,9 +30,15 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
+
+            {ingredients.length > 0 &&
+                <IngredientsList
+                    ingredients={ingredients}
+                    getRecipe={getRecipe}
+                />
+            }
+
+            {recipe && <ClaudeRecipe recipe={recipe} />}
         </main>
     )
 }
