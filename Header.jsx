7ede489bee842fcfad8f6a4bc4ba2d3cf1cfcec2
jihan chowdhury <jihanchowdhury334@gmail.com>
2025-09-26 20:52:54 -0400

    Update Header.jsx

--- Header.jsx
+++ Header.jsx
@@ -1,10 +1,10 @@
-import chefClaudeLogo from "./images/chef-claude-icon.png"
+import chefIcon from "../images/chef-claude-icon.png" // you can rename this image later
 
 export default function Header() {
     return (
         <header>
-            <img src={chefClaudeLogo}/>
-            <h1>Chef Claude</h1>
+            <img src={chefIcon} alt="AI Chef logo"/>
+            <h1>AI Recipe Generator</h1>
         </header>
     )
 }
