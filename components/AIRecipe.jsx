4c7ccaf0fb2e614cc5d097f7097c7b93a56360f7
jihan chowdhury <jihanchowdhury334@gmail.com>
2025-09-26 20:54:14 -0400

    Update and rename ClaudeRecipe.jsx to AIRecipe.jsx

--- components/AIRecipe.jsx
+++ components/AIRecipe.jsx
@@ -1,9 +1,9 @@
 import ReactMarkdown from "react-markdown"
 
-export default function ClaudeRecipe(props) {
+export default function AIRecipe(props) {
     return (
         <section className="suggested-recipe-container" aria-live="polite">
-            <h2>Chef Claude Recommends:</h2>
+            <h2>Your AI-Generated Recipe:</h2>
             <ReactMarkdown>{props.recipe}</ReactMarkdown>
         </section>
     )
